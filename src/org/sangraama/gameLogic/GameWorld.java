5c9c98e7b83acce0af9c6057c66f64893db408cd
Chandima <chandima@ubuntu.ubuntu-domain>
2013-05-16 15:34:18 +0530

    Added JBoxPlayer to GameLogic and channged World to static in GameWorld

--- src/org/sangraama/gameLogic/GameWorld.java
+++ src/org/sangraama/gameLogic/GameWorld.java
@@ -8,9 +8,11 @@ import org.sangraama.asserts.Player;
 import org.sangraama.common.Constants;
 
 public class GameWorld {
-  private World world;
+  static World world;
   private ArrayList<Player> players = null;
-
+  private int WIDTH=600;
+  private int HEIGHT=600;
+  
   public GameWorld() {
     this.world = new World(Constants.gravity, Constants.doSleep);
     this.players = new ArrayList<Player>();
@@ -40,4 +42,39 @@ public class GameWorld {
   public void pushUpdate() {
 
   }
+  
+//Convert a JBox2D x coordinate to a JavaFX pixel x coordinate
+  public float toPixelPosX(float posX) {
+      float x = WIDTH*posX / 100.0f;
+      return x;
+  }
+   
+  //Convert a JavaFX pixel x coordinate to a JBox2D x coordinate
+  public float toPosX(float posX) {
+      float x =   (posX*100.0f*1.0f)/WIDTH;
+      return x;
+  }
+   
+  //Convert a JBox2D y coordinate to a JavaFX pixel y coordinate
+  public float toPixelPosY(float posY) {
+      float y = HEIGHT - (1.0f*HEIGHT) * posY / 100.0f;
+      return y;
+  }
+   
+  //Convert a JavaFX pixel y coordinate to a JBox2D y coordinate
+  public float toPosY(float posY) {
+      float y = 100.0f - ((posY * 100*1.0f) /HEIGHT) ;
+      return y;
+  }
+   
+  //Convert a JBox2D width to pixel width
+  public float toPixelWidth(float width) {
+      return WIDTH*width / 100.0f;
+  }
+   
+  //Convert a JBox2D height to pixel height
+  public  float toPixelHeight(float height) {
+      return HEIGHT*height/100.0f;
+  }
+  
 }
