ee29cee19db7679c06cb99b7b9d9b5f8b9b0feaa
gihankarunarathne <gckarunarathne@gmail.com>
2013-05-18 21:04:42 +0530

    Add player delta protocol class

---
+++ src/org/sangraama/controller/clientprotocol/PlayerDelta.java
@@ -0,0 +1,21 @@
+package org.sangraama.controller.clientprotocol;
+
+public class PlayerDelta {
+    private int dx = 0, dy = 0;
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+}
