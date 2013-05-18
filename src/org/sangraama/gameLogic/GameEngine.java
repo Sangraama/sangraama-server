d65460b943ecefa31e3dfe0fc8bf30f832902dd7
gihankarunarathne <gckarunarathne@gmail.com>
2013-05-18 14:28:07 +0530

    Refactor code,remove build/*

--- src/org/sangraama/gameLogic/GameEngine.java
+++ src/org/sangraama/gameLogic/GameEngine.java
@@ -11,13 +11,12 @@ public enum GameEngine implements Runnable {
     INSTANCE;
     // Debug
     private String TAG = "Game Engine :";
-    // private GameEngine gameEngine = null;
+    
     private World world = null;
     private boolean execute = true;
     private boolean isNewPlayerAvai = false;
     private ArrayList<Player> playerList = null;
     private ArrayList<Player> newPlayerQueue = null;
-    static int i=0;
 
     // this method only access via class
     GameEngine() {
@@ -26,12 +25,18 @@ public enum GameEngine implements Runnable {
 	this.playerList = new ArrayList<Player>();
 	this.newPlayerQueue = new ArrayList<Player>();
     }
-
-    /*
-     * public GameEngine getInstance() { if (gameEngine == null) { gameEngine =
-     * new GameEngine(); System.out.println(TAG + "Create a new Game Engine"); }
-     * return gameEngine; }
-     */
+    
+    @Override
+    public void run() {
+	System.out.println(TAG + "GameEngine Start running..");
+	init();
+	while (execute) {
+	    update();
+	    world.step(1, Constants.velocityIterations,
+		    Constants.positionIterations);
+	    pushUpdate();
+	}
+    }
 
     public void init() {
 
@@ -57,21 +62,7 @@ public enum GameEngine implements Runnable {
 	for (Player player : playerList) {
 	    //System.out.println(TAG + player.getUserID() + " Sending player updates");
 	    player.sendUpdate();
-	    //if( i++ > 100 ) execute =false;
 	}
-    }
-
-    @Override
-    public void run() {
-	System.out.println(TAG + "GameEngine Start running..");
-	init();
-	while (execute) {
-	    update();
-	    world.step(1, Constants.velocityIterations,
-		    Constants.positionIterations);
-	    pushUpdate();
-	}
-
     }
 
     public void stopGameWorld() {
