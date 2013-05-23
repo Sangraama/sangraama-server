ea84b5c07398da8a28d6d79fe9cc97efdba7fa36
gihankarunarathne <gckarunarathne@gmail.com>
2013-05-23 13:42:48 +0530

    Fixed Player conflicts

--- src/org/sangraama/coordination/ServerHandler.java
+++ src/org/sangraama/coordination/ServerHandler.java
@@ -14,6 +14,17 @@ public enum ServerHandler {
 	String URL="";
 	int port=0;
 	
+	public ServerLocation(String url,int port){
+	    this.URL = url;
+	    this.port = port;
+	}
 	
+	public String getServerURL() {
+	    return this.URL;
+	}
+	
+	public int getServerPort() {
+	    return this.port;
+	}
     }
 }
