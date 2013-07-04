c1dc13feab758adf7acff38601b7674e37505111
chandima-dileepa <chandima.dileepa@gmail.com>
2013-07-04 10:51:24 +0530

    Added public key and private key as a text file to the src folder. Added VerifyMsg to verify the signed message. Did some changes to the SignMsg.

--- src/org/sangraama/util/SignMsg.java
+++ src/org/sangraama/util/SignMsg.java
@@ -1,45 +1,37 @@
 package org.sangraama.util;
 
+import java.io.FileInputStream;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.security.Signature;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Properties;
 
-public class SignMsg {
+public enum SignMsg {
 
-    private Properties prop;
+    INSTANCE;
+    private byte[] signature;
+    private String TAG = "SignMsg : ";
 
-    public void signMessage(String messsage) {
+    public byte[] signMessage(String messsage) {
 
         try {
-            prop = new Properties();
-            this.prop.load(getClass().getResourceAsStream("/sangraamaserver.properties"));
-            byte[] encKey= prop.getProperty("privatekey").getBytes();
-
+            FileInputStream keyfis = new FileInputStream("src/PrivateKey.txt");
+            byte[] encKey = new byte[keyfis.available()];
+            keyfis.read(encKey);
+            keyfis.close();
             PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(encKey);
             KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
             PrivateKey priKey = keyFactory.generatePrivate(privKeySpec);
-
             Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
-
             dsa.initSign(priKey);
-            byte[] msg=messsage.getBytes();
-            dsa.update(msg);
-            byte[] signature=dsa.sign();
-            
-            encKey= prop.getProperty("publickey").getBytes();
-            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
-            PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
-            dsa.initVerify(pubKey);
-            
-            boolean verifies = dsa.verify(signature);
-            System.out.println("signature verifies: " + verifies);
-            
+            dsa.update(messsage.getBytes());
+            signature = dsa.sign();
+            System.out.println(TAG + "Done signing the message");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return signature;
     }
 }
