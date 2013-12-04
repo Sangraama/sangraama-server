3f28a7c51dbfd6a30d5b540ff33e38ad50b4d542
Dissanayake <ooopsamila@gmail.com>
2013-12-04 08:49:08 +0530

    added shoot, boundry creation map creation changes to master

--- src/main/java/org/sangraama/assets/Player.java
+++ src/main/java/org/sangraama/assets/Player.java
@@ -94,7 +94,7 @@ public abstract class Player extends AbsPlayer {
          */
         this.x = this.body.getPosition().x;
         this.y = this.body.getPosition().y;
-        this.oldAngle = this.body.getAngle();
+        this.oldAngle = this.body.getAngle()%360;
         // Check whether player is inside the tile or not
         /*
          * Gave this responsibility to client if (!this.isInsideMap(this.x, this.y)) {
@@ -292,28 +292,33 @@ public abstract class Player extends AbsPlayer {
         if (s == 1) {
             float x = this.body.getPosition().x;
             float y = this.body.getPosition().y;
-            if (0 <= this.angle && this.angle <= 90) {
-                float ang = this.angle * Constants.TO_RADIANS;
+            float bulletAngle = this.angle % 360;
+            if (bulletAngle < 0) {
+                bulletAngle += 360;
+            }
+
+            if (0 <= bulletAngle && bulletAngle <= 90) {
+                float ang = bulletAngle * Constants.TO_RADIANS;
                 float rX = (float) (r * Math.cos(ang));
                 float rY = (float) (r * Math.sin(ang));
                 x = x + rX;
                 y = y + rY;
-            } else if (90 <= this.angle && this.angle <= 180) {
-                float ang = (180 - this.angle) * Constants.TO_RADIANS;
+            } else if (90 <= bulletAngle && bulletAngle <= 180) {
+                float ang = (180 - bulletAngle) * Constants.TO_RADIANS;
                 float rX = (float) (r * Math.cos(ang));
                 float rY = (float) (r * Math.sin(ang));
 
                 x = x - rX;
                 y = y + rY;
-            } else if (180 <= this.angle && this.angle <= 270) {
-                float ang = (this.angle - 180) * Constants.TO_RADIANS;
+            } else if (180 <= bulletAngle && bulletAngle <= 270) {
+                float ang = (bulletAngle - 180) * Constants.TO_RADIANS;
                 float rX = (float) (r * Math.cos(ang));
                 float rY = (float) (r * Math.sin(ang));
 
                 x = x - rX;
                 y = y - rY;
-            } else if (270 <= this.angle && this.angle <= 360) {
-                float ang = (360 - this.angle) * Constants.TO_RADIANS;
+            } else if (270 <= bulletAngle && bulletAngle <= 360) {
+                float ang = (360 - bulletAngle) * Constants.TO_RADIANS;
                 float rX = (float) (r * Math.cos(ang));
                 float rY = (float) (r * Math.sin(ang));
 
@@ -431,7 +436,7 @@ public abstract class Player extends AbsPlayer {
     }
 
     public void setAngle(float a) {
-        this.angle = a;
+        this.angle = a%360;
         // log.info(TAG + userID + "  set angle : " + a + " > " + this.angle);
     }
 
