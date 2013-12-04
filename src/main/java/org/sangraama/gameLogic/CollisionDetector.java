43e756270294005f84bcf7b0147c0833a4ef6560
chandima-dileepa <chandima.dileepa@gmail.com>
2013-12-05 00:00:28 +0530

    Added comments to CollisionDetector and CollisionManager.

--- src/main/java/org/sangraama/gameLogic/CollisionDetector.java
+++ src/main/java/org/sangraama/gameLogic/CollisionDetector.java
@@ -5,10 +5,22 @@ import org.jbox2d.callbacks.ContactListener;
 import org.jbox2d.collision.Manifold;
 import org.jbox2d.dynamics.contacts.Contact;
 
+/**
+ * This class detects collision of the game world. After detecting collisions send the collisions to
+ * CollisionManager to apply game logic on collisions.
+ * 
+ * @author Dileepa Rajaguru
+ * 
+ */
+
 public class CollisionDetector implements ContactListener {
 
     CollisionManager sanCollisionManager;
 
+    /**
+     * Identify the collisions when two bodies in the world start to collide.Then pass that
+     * collision to the CollisionManager.
+     */
     public CollisionDetector() {
         sanCollisionManager = CollisionManager.INSTANCE;
     }
@@ -20,7 +32,7 @@ public class CollisionDetector implements ContactListener {
 
     @Override
     public void endContact(Contact contact) {
-        
+
     }
 
     @Override
--- src/main/java/org/sangraama/gameLogic/CollisionManager.java
+++ src/main/java/org/sangraama/gameLogic/CollisionManager.java
@@ -17,6 +17,13 @@ import org.sangraama.gameLogic.queue.BulletQueue;
 import org.sangraama.jsonprotocols.transfer.ScoreChangeTransferReq;
 import org.slf4j.*;
 
+/**
+ * This class process the collisions occur in the game world. Then apply the game logic on relevant
+ * entities.
+ * 
+ * @author Dileepa Rajaguru
+ * 
+ */
 public enum CollisionManager implements Runnable {
     INSTANCE;
     public static final Logger log = LoggerFactory.getLogger(CollisionManager.class);
@@ -26,6 +33,9 @@ public enum CollisionManager implements Runnable {
     private volatile boolean isUpdate = false;
     private List<Contact> collisionList;
 
+    /**
+     * Initialize Collision Manager.
+     */
     CollisionManager() {
         this.gameEngine = GameEngine.INSTANCE;
         this.bulletQueue = BulletQueue.INSTANCE;
@@ -37,6 +47,12 @@ public enum CollisionManager implements Runnable {
         return this.isRun;
     }
 
+    /**
+     * Add the collisions occurred to the Collision Manager.
+     * 
+     * @param col
+     *            collision in the world
+     */
     public synchronized void addToCollisionList(Contact col) {
         this.collisionList.add(col);
         this.isUpdate = true;
@@ -58,6 +74,13 @@ public enum CollisionManager implements Runnable {
 
     }
 
+    /**
+     * Process the collision occurred in the world. Identify what entities were collided and apply
+     * game logic on them.
+     * 
+     * @param collision
+     *            collision in the collisionList
+     */
     private void processCollisions(Contact collision) {
         // System.out.println(TAG + "Processing collisions ## ## ");
         if (collision.getFixtureA().getUserData().getClass() == Ship.class
@@ -118,12 +141,26 @@ public enum CollisionManager implements Runnable {
         }
     }
 
+    /**
+     * Apply game logic if multiple ships collided together.
+     * 
+     * @param ships
+     *            Ship collided
+     */
     private void processShipsCollision(Ship... ships) {
         for (Ship ship : ships) {
             reduceShipHealth(ship.getUserID(), -0.2f);
         }
     }
 
+    /**
+     * Apply the game logic on the victim ship. Apply the game logic on the shooter.
+     * 
+     * @param ship
+     *            Victim Ship
+     * @param bullet
+     *            Bullet hit
+     */
     private void processBulletShipCollition(Player ship, Bullet bullet) {
         boolean playerInServer = false;
         reduceShipHealth(ship.getUserID(), -1f);
@@ -141,6 +178,14 @@ public enum CollisionManager implements Runnable {
         }
     }
 
+    /**
+     * Reduce the health of the victim ship by valChange
+     * 
+     * @param userID
+     *            userID of the victim
+     * @param valChange
+     *            change in the health
+     */
     private void reduceShipHealth(float userID, float valChange) {
         for (Player player : gameEngine.getPlayerList()) {
             if (player.getUserID() == userID) {
@@ -150,6 +195,14 @@ public enum CollisionManager implements Runnable {
         }
     }
 
+    /**
+     * Reward the shooter who was success. Change the score of the shooter by scoreChange.
+     * 
+     * @param shipID
+     *            userID of the shooter
+     * @param scoreChange
+     *            change in the score of the shooter
+     */
     private void sendScoreChangeEventFromDummy(long shipID, int scoreChange) {
         List<DummyPlayer> dummyList = AOIEngine.INSTANCE.getDummyList();
         for (DummyPlayer dummyPlayer : dummyList) {
